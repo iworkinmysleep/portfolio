dba98c302c9074260c49f7f80db6593e70786afb
Jason Hinton <jason_hinton@att.net>
2021-07-13 12:53:21 -0400

    better project descriptions

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -25,7 +25,7 @@ const Projects = () => {
 					<ProjectsIcon src={puterStuff} />
 					<ProjectsH2>PuterStuff</ProjectsH2>
 					<ProjectsP>
-						Full stack eCommerce app built with MERN stack. <br />{" "}
+						Full stack eCommerce app built with MERN stack. User auth using JWT.  Admin users have crud functionality with products and users.<br />{" "}
 						<ProjectLink
 							target="blank"
 							href="https://puterstuff.herokuapp.com/">
@@ -43,7 +43,7 @@ const Projects = () => {
 					<ProjectsH2>How-To</ProjectsH2>
 					<ProjectsP>
 						A simple app that allows users to view, add, update, and delete life
-						hacks. (back end developer)
+						hacks. (back end developer) Node, Express, Knex, SQLite
 					</ProjectsP>
 					<GitHubLink
 						target="_blank"
@@ -55,8 +55,7 @@ const Projects = () => {
 					<ProjectsIcon src={telenet} />
 					<ProjectsH2>Telenet-Data</ProjectsH2>
 					<ProjectsP>
-						I built this app after I was tasked with taking inventory notes in a
-						Word document at my current job. <br />
+						I built this small app to help solve a problem for a telecom company that needed to keep inventory notes. React, Node, Express, MongoDB Bootstrap <br />
 						<ProjectLink
 							target="blank"
 							href="https://telenet-data.herokuapp.com/">
